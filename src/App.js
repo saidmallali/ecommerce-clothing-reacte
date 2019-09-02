aee932c933818aaf09140fb32ccbac59e836a00c
saidmallali <said3.labriki@gmail.com>
2019-09-02 02:24:56 +0200

    adding the header component and styling it

--- src/App.js
+++ src/App.js
@@ -3,20 +3,18 @@ import { Switch, Route} from 'react-router-dom';
 
 import './App.css';
 
-import HomePage from './pages/homepage/homepage.component'
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './compononents/shop/shop.component'
 
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-);
+import Header from './compononents/header/header.component'
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path="/" component={HomePage}/>
-        <Route exact path="/hats" component={HatsPage}/>
+        <Route exact path="/shop" component={ShopPage}/>
       </Switch>
       
     </div>
